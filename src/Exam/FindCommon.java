3837af2dad492e004ad55dc1ca9538937a057cd2
Natseol <jewelbox00@gmail.com>
2023-09-25 17:41:33 +0900

    수정 : 답이 마음에 안들어서 추가

--- src/Exam/FindCommon.java
+++ src/Exam/FindCommon.java
@@ -1,16 +1,35 @@
 package Exam;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 public class FindCommon {
 	public static void main(String[] args) {
 
 		int[] arr1 = {1,3,4,5};
-		int[] arr2 = {3,4,6,10};
-		int[] arr3 = {6,20,20};
+		int[] arr2 = {3,4,5,6,10};
+		int[] arr3 = {1,20,20};
 		
-		findCommonItem(arr1, arr2, arr3);
+		findCommonItem2(arr1, arr2, arr3);
 	}
+	
+	static void findCommonItem2(int[] arr1, int[] arr2, int[] arr3) {
+		int[][] arrAll = {arr1, arr2, arr3};
+		TreeSet<Integer> treeSet = new TreeSet<>();
+		
+		for (int i = 0; i < arrAll.length; i++) {
+			for (int j = i+1; j < arrAll.length-1; j++) {
+				for (int k = 0; k<arrAll[j].length ; k++) {
+					if(Arrays.binarySearch(arrAll[i], arrAll[j][k])>=0) {
+						treeSet.add(arrAll[j][k]);
+					}
+				}
+			}
+		}
+		treeSet.forEach(System.out::println);	
+	}
+	
+	
 	
 	static void findCommonItem(int[] arr1, int[] arr2, int[] arr3) {
 		int[][] arrAll = {arr1, arr2, arr3};
